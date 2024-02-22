467fcb244306d88fae2b8506622dcb9ff4531a33
usr-0123 <fixedzone365@gmail.com>
2024-02-22 16:11:01 +0300

    The skeleton structure of the landing page

---
+++ src/layouts/Sidebar.jsx
@@ -0,0 +1,13 @@
+//Styling file
+import './Sidebar.scss'
+
+//import layouts
+
+const Sidebar = () => {
+    return(
+        <>
+        This is the Sidebar
+        </>
+    )
+}
+export default Sidebar
