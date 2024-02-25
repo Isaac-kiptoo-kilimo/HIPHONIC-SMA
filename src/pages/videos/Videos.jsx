2637eebe8d5d3a5bb9baa179d293c35c0fd50ebf
usr-0123 <fixedzone365@gmail.com>
2024-02-25 06:53:26 -0800

    done the video post frontend page

---
+++ src/pages/videos/Videos.jsx
@@ -0,0 +1,32 @@
+//import stylefile
+import './Videos.scss'
+
+//import components
+import { VideoPost } from "../../components/videos/VideoPost";
+
+//import icons
+import { CiSearch } from "react-icons/ci";
+
+const Videos = () => {
+    return (
+    <div className="Videos">
+        <div className="videosTop">
+            <div className="videosTopHeader">
+                <div style={{fontWeight:"700"}}>Video</div>
+                <div><CiSearch /></div>
+            </div>
+            <div className="videosTopContent">
+                <div className="videosTopContentheader">
+                    <div>Categories to explore</div>
+                    <div style={{color:"#2563EB"}}>See all</div>
+                </div>
+                <div className="videosTopContentCards">
+                    <VideoPost />
+                </div>
+            </div>
+        </div>
+        <div className="videosBottom"></div>
+    </div>
+    )
+}
+export default Videos;
