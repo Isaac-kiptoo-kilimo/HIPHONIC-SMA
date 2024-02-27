602cb02a3b82a07395bf9eeeee074e28f53a6408
usr-0123 <fixedzone365@gmail.com>
2024-02-27 21:49:56 +0300

    Fetching the posts of the logged in User

--- src/components/profile/ProfileStatusPost.jsx
+++ src/components/profile/ProfileStatusPost.jsx
@@ -1,54 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import { useGetPostsQuery } from '../../features/posts/postApi.js';
+import { useGetPostQuery } from '../../features/posts/postApi.js';
 
-//icons
-import { TbMoodSmile } from "react-icons/tb";
-import { IoIosLink } from "react-icons/io";
+// Icons
 import { FaHeart } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
 import { GoShareAndroid } from "react-icons/go";
 
-//import assets
-import Avatar from '../../assets/Avatar.png'
-
-//stylefile
+// Styles
 import './ProfileStatusPost.scss'
 
-//import components
-import User from "../profile/userInfocard.jsx"
+const ProfileStatusPost = ({ loggedInUser, postId }) => {
+    const { data: post, error, isLoading, isError, isFetching } = useGetPostQuery(postId);
+    const [userDetails, setUserDetails] = useState(loggedInUser);
 
-const ProfileStatusPost = () => {
-    const user = JSON.parse(localStorage.getItem('loggedInUser'));
-    
+    useEffect(() => {
+        setUserDetails(loggedInUser);
+    }, [loggedInUser]);
+
     return (
         <div className="profileStatusPost">
-            <div>
-                <User />
+            <div className='profileStatusPostHeader'>
+                <div className="profilePic">
+                    {/* <img src={userDetails.user.profileImage} alt="Profile" /> */}
+                </div>
+                <div className="user">
+                    <p className="username">userDetails.user.Username</p>
+                    <p className="postDate">56 mins ago</p>
+                </div>
             </div>
             <div className='profileStatusPostTextContent'>
-                    <div>
-                        <p>content</p>
-                    </div>
-            </div>
-            <div className='profileStatusPostImageContent'>
-                2 fetched pics
-            </div>
-            <div className='profileStatusPostInteraction'>
-
-                <div className="like"><FaHeart /> Likes</div>
-                <div className="comment"><AiOutlineMessage/> Comments</div>
-                <div className="share"><GoShareAndroid/> 201 Share</div>
+                <div>
+                    {isError ? (
+                        <p>Error fetching post</p>
+                    ) : isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        post && (
+                            <div key={post.id}>
+                                <p>{post.content}</p>
+                                {/* Render post likes and comments here */}
+                                <div className='profileStatusPostInteraction'>
+                                    <div className="like"><FaHeart /> {post.likes} Likes</div>
+                                    <div className="comment"><AiOutlineMessage /> {post.comments} Comments</div>
+                                    <div className="share"><GoShareAndroid /> {post.shares} Shares</div>
+                                </div>
+                            </div>
+                        )
+                    )}
+                </div>
             </div>
             <div className='profileStatusPostComment'>
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     placeholder='Write your message...' />
-                <div className="btnIcons">
-                    <TbMoodSmile />
-                    <IoIosLink />
-                </div>
             </div>
         </div>
     )
 }
+
 export default ProfileStatusPost;
